84120cfd09bdd0b40520104d797330f0172e61e0
murpyh <51100244+iimurpyh@users.noreply.github.com>
2026-01-06 11:07:09 -0500

    new datalink system

--- commands/datalink.js
+++ commands/datalink.js
@@ -1,140 +1,36 @@
-const datalinks = require('../ss-data/datalinks (raw).json');
-const parts = require('../ss-data/parts.json');
-const tools = require('../ss-data/tools.json')
+const datalinks = require('../scarlet-skies-data/datalinks.json');
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
-// Pages where rich text should not purged. This will almost certainly only ever be Book
-const ignoreRichtextList = [
-    "Book"
-];
-
-// Pages that are missing things like an article title.
-const dontFindTagsList = [
-    "ReleaseNotes", "Rules"
-];
-
-const dontFindBottomList = [
-    "Bot", "Programming", "Storms", "Rules"
-];
-
-function getArticleEmbed(i, page) {
-    console.log(i);
-    var hasPages = false;
-    var text = datalinks[i];
-    if (text.indexOf("\t\n") == 0) {
-        text = text.slice(2);
-    }
+function getArticleEmbed(articleInfo, page) {
+    const pageCount = articleInfo.body.length;
+    const hasPages = pageCount > 1;
+    const creditCost = articleInfo.price;
+    const articleEmbed = new EmbedBuilder().setTitle(articleInfo.title);
     
-    var data;
-    var type;
-    var title = i;
-    if (!dontFindTagsList.find(elm => elm == i)) {
-        type = text.slice(0, text.indexOf(":"));
-        subtitle = type.charAt(0) + type.toLowerCase().slice(1);
-    } else {
-        type = "Page";
-        subtitle = "Page";
-    }
-    
-    if (type == "PART" || type == "SUBSTANCE") {
-        data = parts[i];
-    } else if (type == "TOOL") {
-        // special case for tools here because their name will sometimes contain spaces 
-        for (let tool in tools) {
-            if (tool.toLowerCase().replace(' ', '') == i.toLowerCase()) {
-                title = tool
-                data = tools[tool];
-            }
+    let subtitle = articleInfo.articleType
+    if (creditCost) {
+        if (creditCost == 1) {
+            subtitle += " | 1 Credit"
+        } else {
+            subtitle += " | " + creditCost + " Credits"
         }
     }
 
-    // first newline is the CATEGORY: TITLE thing 
-    // also remove richtext
-    text = text.slice(text.indexOf("\n"))
-    if (!ignoreRichtextList.find(elm => elm == i)) {
-        text = text.replace(/<(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g, '');
+    if (hasPages) {
+        subtitle += " | Page " + (page+1) + "/" + pageCount;
     }
 
-    const articleEmbed = new EmbedBuilder().setTitle(title);
+    const bodyText = articleInfo.body[page];
 
-    var credits
-    if (data && data.Price) {
-        credits = data.Price;
-    } else if (data && data.Value) {
-        credits = data.Value;
-    }
-    if (credits == 1) {
-        subtitle += " | " + credits + " Credit"
-    } else if (credits) {
-        subtitle += " | " + credits + " Credits"
-    }
+    const description = "**" + subtitle + "**\n\n" + bodyText;
+    articleEmbed.setDescription(description)
     
-    var finalText = text;
-    
-
-    if (!dontFindBottomList.find(elm => elm == i) && !dontFindTagsList.find(elm => elm == i)) {
-        finalText = text.substring(0, text.lastIndexOf("\n\n"));
+    for (articleField of articleInfo.footerData) {
+        console.log(articleField.name, articleField.desc);
+        articleEmbed.addFields({ name: articleField.name, value: String(articleField.desc), inline: true });
     }
 
-    var index = 0;
-    text.split("\n").forEach(line => {
-        if (!(line.indexOf("-") == 1 || line.length < 70 || line.indexOf(":") > 0 || line.length == 0 || line.toUpperCase() === line)) {
-            if (finalText.charAt(index + line.length + 1) != "\n") {
-                finalText = finalText.substring(0, index + line.length) + " " + finalText.substring(index + line.length + 1);
-            }
-        }
-        if (/[a-zA-Z]/g.test(line) && line.replace(/ *\([^)]*\) */g, "").toUpperCase() === line.replace(/ *\([^)]*\) */g, "")) {
-            finalText = finalText.substring(0, index) + "**" + finalText.substring(index, index + line.length) + "**" + finalText.substring(index + line.length);
-            index += 4;
-        }
-        index += line.length + 1;
-    })
-    
-    //finalText = finalText.replace(/(?<!\n)\n(?!\n|-)/g, ' ');
-    var lastPage = 0;
-    
-    if (finalText.indexOf("#pgbreak#") > 0) {
-        lastPage = [...finalText.matchAll("#pgbreak#")].length;
-        subtitle += ' | ' + (parseInt(page) + 1) + '/' + (lastPage + 1);
-        hasPages = true;
-        finalText = [...finalText.split("#pgbreak#")][page];
-    }
-    
-    finalText = finalText.replaceAll('#br#', '\n');
-
-    articleEmbed.setDescription("**" + subtitle + "**" + finalText)
-
-    if (data) {
-        
-
-        if (data.Malleability != undefined) {
-            if (data.Malleability > 0) {
-                articleEmbed.addFields({ name: "Malleability", value: (data.Malleability**2).toString(), inline: true }); // malleability**2 is due to the technical malleability being the root of its actual malleability, but it is more far useful to state the actual malleability visible ingame
-            } else {
-                articleEmbed.addFields({ name: "Fixed Size", value: data.DefaultSize.X + ", " + data.DefaultSize.Y + ", " + data.DefaultSize.Z, inline: true });
-            }
-            
-        }
-        if (data.Recipe) {
-            articleEmbed.addFields({ name: "Recipe", value: data.Recipe.join(", "), inline: true });
-        }
-    }
-    
-    if (!dontFindBottomList.find(elm => elm == i) && !dontFindTagsList.find(elm => elm == i)) {
-        var tabList = [...text.substring(text.lastIndexOf("\n\n")).matchAll(":")];
-
-        for (var num in tabList) {
-            var index = tabList[num].index + text.substring(0, text.lastIndexOf("\n\n")).length;
-            console.log(text.substring(index).indexOf('\n'))
-            var name = text.substring(text.substring(0, index).lastIndexOf('\n') + 1, index);
-            var val = text.substring(index + 2, text.substring(index).indexOf('\n') + text.substring(0, index).length);
-            if (name != "Requires" && name != "Malleability") {
-                articleEmbed.addFields({ name: name, value: val, inline: true });
-            }
-        }
-    }
-
-    return [articleEmbed, hasPages, lastPage];
+    return [articleEmbed, hasPages, page >= pageCount - 1];
 }
 
 
@@ -151,52 +47,50 @@ module.exports = {
     ],
 
 
-    onRun: async (client, interaction, arguments) => {
-        article = arguments[0];
+    onRun: async (client, interaction, commandArguments) => {
+        const article = commandArguments[0];
 
         if (!article) {
-            await interaction.reply("You have to include the name of an article. Use the list command to see the available options.");
+            await interaction.reply("You have to include the name of an article. Use the `list` command to see the available options.");
             return;
         }
+
+        const searchTerm = article.toLowerCase();
         
-        for (let i in datalinks) {
-            if (i.toLowerCase() == article.toLowerCase()) {
-                var [embed, hasPages] = getArticleEmbed(i, 0);
-                console.log(hasPages);
-                if (hasPages) {
-                    const row = new ActionRowBuilder()
-                        .addComponents(
-                            new ButtonBuilder()
-                                .setCustomId('datalink back ' + i + ' ' + 0)
-                                .setEmoji('⬅️')
-                                .setStyle(ButtonStyle.Primary)
-                                .setDisabled(true),
-                            new ButtonBuilder()
-                                .setCustomId('datalink forward ' + i + ' ' + 0)
-                                .setEmoji('➡️')
-                                .setStyle(ButtonStyle.Primary)
-                        );
-                    await interaction.reply({ embeds: [embed], components: [row] });
-                } else {
-                    await interaction.reply({ embeds: [embed] });
-                }
-                return;
-                
+        if (searchTerm in datalinks) {
+            var [embed, hasPages] = getArticleEmbed(datalinks[searchTerm], 0);
+            if (hasPages) {
+                const row = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('datalink back ' + searchTerm + ' ' + 0)
+                            .setEmoji('⬅️')
+                            .setStyle(ButtonStyle.Primary)
+                            .setDisabled(true),
+                        new ButtonBuilder()
+                            .setCustomId('datalink forward ' + searchTerm + ' ' + 0)
+                            .setEmoji('➡️')
+                            .setStyle(ButtonStyle.Primary)
+                    );
+                await interaction.reply({ embeds: [embed], components: [row] });
+            } else {
+                await interaction.reply({ embeds: [embed] });
             }
+            return;
         }
         await interaction.reply("Article not found.")
     },
 
     onButton: async (client, interaction) => {
-        [command, direction, article, page] = interaction.customId.split(" ");
+        let [command, direction, article, page] = interaction.customId.split(" ");
         page = parseInt(page);
         if (direction == "forward") {
             page += 1;
         } else if (direction == "back") {
             page -= 1;
         }
-        console.log(article, interaction.customId);
-        var [embed, hasPages, lastPage] = getArticleEmbed(article, page);
+
+        const [embed, hasPages, isLastPage] = getArticleEmbed(datalinks[article], page);
         const row = new ActionRowBuilder()
         .addComponents(
             new ButtonBuilder()
@@ -208,7 +102,7 @@ module.exports = {
                 .setCustomId('datalink forward ' + article + ' ' + page)
                 .setEmoji('➡️')
                 .setStyle(ButtonStyle.Primary)
-                .setDisabled(page == lastPage)
+                .setDisabled(isLastPage)
         );
         await interaction.update({ embeds: [embed], components: [row] });
     }
---
+++ scarlet-skies-data/generateDatalinks.js
@@ -0,0 +1,209 @@
+const { init } = require('server-text-width');
+const fs = require('fs');
+
+const TEXT_WIDTH_LOOKUP_TABLE = {
+    "Arimo|11px|700|0": "AANANANANANANANANADDDDDDDDDDNANANANANANANANANANANANANANADDDDDDDDDDDqFNGHGHJyH8CnDqDqESGaDDDqDDDDGHGHGHGHGHGHGHGHGHGHDqDqGaGaGaGtKuH8H8H8H8HVGtIjH8DDGHH8GtJKH8IjHVIjH8HVGtH8HVKYHVHVGtDqDDDqGaGHDqGHGtGHGtGHDqGtGtDDDDGHDDJyGtGtGtGtESGHDqGtGHIjGHGHFgESDFESGaNANANANANANADDNANANANANANANANANANANANANANANANANANANANANANANANANANADDDqGHGHGHGHDFGHDqIGEEGHGaAAIGGEEZGCDqDqDqGmGHDqDqDqEBGHJKJKJKGtH8H8H8H8H8H8LAH8HVHVHVHVDDDDDDDDH8H8IjIjIjIjIjGaIjH8H8H8H8HVHVGtGHGHGHGHGHGHJyGHGHGHGHGHDDDDDDDDGtGtGtGtGtGtGtGCGtGtGtGtGtGHGtGHH8GHH8GHH8GHH8GHH8GHH8GHH8GHH8H5H8GtHVGHHVGHHVGHHVGHHVGHIjGtIjGtIjGtIjGtH8GtH8GtDDDDDDDDDDDDDDDDDDDDIoGHGHDDH8GHGHGtDDGtDDGtEOGtFRGtDDH8GtH8GtH8GtHzH8GtIjGtIjGtIjGtLAKYH8ESH8ESH8ESHVGHHVGHHVGHHVGHGtDqGtFRGtDqH8GtH8GtH8GtH8GtH8GtH8GtKYIjHVGHHVGtFgGtFgGtFgDDGtJMH5GtH5GtH8H8GHH8JNH5GtGqHVH+G4GtGHIjHAKODDDDH8GHDDGHK6H8GtIjJYH0JwILImGtHVHVGHGmD9DqGtDqGtJIH9I0H8IdGHGtFgGtGtFyFyGHGHFdGXGtDFFdGbDqOqNcMNM1JyGHOELAJyH8GHDDDDIjGtH8GtH8GtH8GtH8GtH8GtGHH8GHH8GHLAJyIjGtIjGtH8GHIjGtIjGtGtFyDDOqNcMNIjGtLXHUH8GtH8GHLAJyIjGtH8GHH8GHHVGHHVGHDDDDDDDDIjGtIjGtH8ESH8ESH8GtH8GtHVGHGtDqGWFtH8GtHuJNG7G7GtFgH8GHHVGHIjGtIjGtIjGtIjGtHVGHFeJKFkDDKeKeH8H8GHGtGtGHFgHGFUH8H8HVHVGHGHDDIhGtH8ESHVGHGHGtGtGtGHGHGtGtGHGHImFdFdHmGODqGtGtGcGHGmGtGtGtDDDDEcDpD6DDGoJyJyJyGtGtGxGtJKJTIQESESESESESESESGaGaGHDqDqDqF7DqDqGtG1GZGHIjGHGSFgHeFyFyGHGHGHGHIjGxGOGcGpFDGHE9GtGHGHLVKXNUI8F4JEJkIiHnF3GMIBIBEDEDCICxCxCxEIFwD9CnFNDDDDDDDqDqEIEIGaGaGaGaDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqDqEBBjEIECEIENENENENENDqDqDqDqFgDqDqDqDqDqDqDqDqDqDqDqDqDqDqEXEXDqAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGaE5G5FhDqDqH5GxNANADqGHGHGHDqGHNANANANADqFHH8DqJYJ9FNNAJENAKMJNDDH8H8GnH5HVGtH8IjDDH8HVJKH8HFIjH8HVNAGmGtHVJCHVI5I0DDHVGxE9GtDDGZGxGtGHGqFNFEGtF8DDGIGHGuGHE5GtIaGzFtHhE6GZH3GVISJTDDGZGtGZJTH8GtGZIdLBIdIQJTHbIjGtH8GHGtE9IRF0I2GtK6JyHuGtHuGpHVHVHEGoIFGXFlE1HbGzGHDDIjFRFRHVGtH8JKIIGzH8H8H8"
+}
+
+const { getTextWidth } = init(TEXT_WIDTH_LOOKUP_TABLE);
+
+// If a line is of this length or longer and ends with a single linebreak,
+// it will be considered a Manual Line Wrap (MLW)
+// Set this string to the shortest example of a line with a MLW you can find
+const manualLineWrapWidth = getTextWidth("It is fairly weak, but is as light as Wood, and so a good material for aircraft and");
+
+// Each match captures an article's name (group #1) and its text body (group #2).
+const regexDatalinksRaw = /(?:local)?(\w*) = \[\[\t?\n?([\S\s]*?)\]\]/g;
+
+// To be run on an article body.
+// Matches the entire article title/header. Group #1 is the article's type
+const regexArticleTypeHeader = /^(PART|TOOL|SYSTEM|SUBSTANCE): .*?\n\n/;
+
+// To be run on an article body.
+// Matches the entire article footer (i.e. the section with fields like Malleability: Good)
+const regexArticleFooter = /(?<=\n\n)(?:\n?.+?:.+?)+(?=\n$)/g;
+
+const regexAlphabeticCharacters = /[a-zA-Z]/g;
+
+const regexParentheticalPhrase = / *\([^)]*\) */;
+
+const partsData = JSON.parse(fs.readFileSync('./scarlet-skies-data/parts.json'));
+const generateOptions = JSON.parse(fs.readFileSync('./scarlet-skies-data/datalinkGenerateOptions.json'));
+
+function toTitleCase(text) {
+    return text.charAt(0).toUpperCase() + text.substring(1).toLowerCase();
+}
+
+function getArticleType(articleTitle, articleBodyText) {
+    const articleTypeMatch = articleBodyText.match(regexArticleTypeHeader);
+
+    if (generateOptions.overrides[articleTitle] && generateOptions.overrides[articleTitle]?.ignoreHeader) {
+        return {
+            articleType: "Page",
+            articleTypeHeaderEndIndex: 0
+        }
+    }
+
+    if (articleTypeMatch === null) {
+        return {
+            articleType: "Page",
+            articleTypeHeaderEndIndex: 0
+        }
+    } else {
+        return {
+            articleType: toTitleCase(articleTypeMatch[1]),
+            articleTypeHeaderEndIndex: articleTypeMatch[0].length
+        };
+    }
+}
+
+function getArticleFooterData(articleTitle, articleBodyText) {
+    const fields = [];
+
+    if (generateOptions.overrides[articleTitle] && generateOptions.overrides[articleTitle]?.ignoreFooter) {
+        return {
+            articleFooterData: fields,
+            articleFooterEndIndex: 0
+        }
+    }
+
+    const articleFooterMatch = articleBodyText.match(regexArticleFooter);
+
+    if (articleFooterMatch === null) {
+        return {
+            articleFooterData: fields,
+            articleFooterEndIndex: 0
+        }
+    } else {
+        const footerText = articleFooterMatch[0];
+        
+        footerText.split('\n').forEach(line => {
+            const split = line.split(':');
+            const fieldName = split[0];
+            const fieldDesc = split[1].substring(1);
+            if (!generateOptions.ignoredFooterFields.find((e) => e === fieldName)) {
+                fields.push({name: fieldName, desc: fieldDesc});
+            }
+        })
+
+        // Plus two for the preceding \n\n, which isn't a part of the match,
+        // and plus one for the ending \n
+        return {
+            articleFooterData: fields,
+            articleFooterEndIndex: footerText.length + 3
+        };
+    }
+}
+
+function stripRichTextTags(text) {
+    return text.replace(/<(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g, '');
+}
+
+function escapeFormattingChars(text) {
+    text = text.replace("\\", "\\\\");
+    text = text.replace("`", "\\`");
+    text = text.replace("*", "\\*");
+    text = text.replace("_", "\\_");
+    return text;
+}
+
+function shouldArticleLineEndBeStripped(line) {
+    if (line.indexOf(':') != -1) return false;
+    return getTextWidth(line) >= manualLineWrapWidth;
+}
+
+function shouldArticleLineBeBold(line) {
+    const lineNoParenPhrase = line.replace(regexParentheticalPhrase, '');
+    return regexAlphabeticCharacters.test(line) && lineNoParenPhrase.toUpperCase() === lineNoParenPhrase;
+}
+
+function roundNumber(number) {
+    return Math.round(number * 10) / 10;
+}
+
+function generateArticle(articleTitle, articleBodyText) {
+    const { articleType, articleTypeHeaderEndIndex } = getArticleType(articleTitle, articleBodyText);
+    const { articleFooterData, articleFooterEndIndex } = getArticleFooterData(articleTitle, articleBodyText);
+
+    const article = {};
+
+    const strippedBodyText = articleBodyText.substring(articleTypeHeaderEndIndex, articleBodyText.length - articleFooterEndIndex);
+    let parsedBodyText = "";
+
+    const splitBody = strippedBodyText.split('\n');
+    const pageSubtitles = generateOptions.overrides[articleTitle]?.pageSubtitles;
+    const pagesArray = [];
+
+    for (let lineNumber = 0; lineNumber < splitBody.length; lineNumber++) {
+        let line = stripRichTextTags(splitBody[lineNumber]);
+        line = escapeFormattingChars(line);
+        
+        if (pageSubtitles && pageSubtitles.find((e) => e === line)) {
+            pagesArray.push(parsedBodyText);
+            parsedBodyText = "";
+        }
+
+        if (shouldArticleLineBeBold(line)) {
+            line = "**" + line + "**";
+        }
+
+        parsedBodyText += line;
+        if (!shouldArticleLineEndBeStripped(line) || splitBody[lineNumber + 1] === '') {
+            parsedBodyText += '\n';
+        } else {
+            if (line.charAt(line.length - 1) != ' ') {
+                parsedBodyText += ' ';
+            }
+        }
+    }
+
+    pagesArray.push(parsedBodyText);
+
+    article.title = articleTitle;
+    article.articleType = articleType;
+    article.body = pagesArray;
+    article.rawBody = strippedBodyText;
+    article.footerData = articleFooterData;
+
+    const partData = partsData[articleTitle];
+    if (partData) {
+        if (partData.Price) article.price = partData.Price;
+        if (partData.Malleability != null) {
+            if (partData.Malleability > 0) {
+                article.footerData.push({
+                    name: "Malleability", desc: partData.Malleability ** 2
+                });
+            } else {
+                
+                article.footerData.push({
+                    name: "Fixed Size", desc: roundNumber(partData.DefaultSize.X) + ", " + roundNumber(partData.DefaultSize.Y) + ", " + roundNumber(partData.DefaultSize.Z)
+                });
+            }
+            
+        }
+        if (partData.Recipe) {
+            article.footerData.push({
+                name: "Recipe", desc: partData.Recipe.join(', ')
+            });
+        }
+    }
+
+    return article;
+}
+
+function generateAllArticles(rawFileText) {
+    const articles = {};
+
+    for (const match of rawFileText.matchAll(regexDatalinksRaw)) {
+        const articleName = match[1];
+        const articleBody = match[2];
+
+        const article = generateArticle(articleName, articleBody);
+        articles[articleName.toLowerCase()] = article;
+    }
+
+    return JSON.stringify(articles, null, 4);
+}
+
+const rawFileText = fs.readFileSync('./scarlet-skies-data/datalinksRaw.txt').toString();
+fs.writeFileSync('./scarlet-skies-data/datalinks.json', generateAllArticles(rawFileText));
